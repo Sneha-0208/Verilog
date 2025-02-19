ba98efddfb58b1914250b6cb8463d787b1c235bc
Sneha Rani <146618463+Sneha-0208@users.noreply.github.com>
2025-02-19 19:56:08 +0530

    Update D_latch.v

--- D_latch.v
+++ D_latch.v
@@ -30,3 +30,37 @@ module d_latch(d, clk,rst,q);
        	 q <= d;
    end
 endmodule
+
+// Testbench for D latch and flip flop
+
+module tb;
+  reg d,clk,rst;
+  wire q;
+  
+  d_latch dut(d,clk,rst,q);
+  
+  initial
+    begin
+    clk=0;
+  d=0;
+  rst=0;
+  #1rst=1;
+  #2d=1;
+  #4rst=0;
+  #4d=0;
+  #8d=1;
+  #7d=0;
+  
+  #5 $finish;
+  end
+  
+  always
+    #2clk=~clk;
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(0,clk,d,rst,q);
+    end
+endmodule
+
